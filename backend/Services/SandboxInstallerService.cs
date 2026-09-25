a80c223b484e9d883102739170a14f32c5f236e5
Christiaan Du Toit <christiaandutoit2006@gmail.com>
2026-09-25 18:48:19 +0200

    fix(sandbox): remove /LimitAccess from feature enablement script and show execution stages

--- backend/Services/SandboxInstallerService.cs
+++ backend/Services/SandboxInstallerService.cs
@@ -43,10 +43,12 @@ namespace FileAnomalyScanner.Services
             // Exact batch script required to enumerate, stage, and enable hidden Sandbox packages on Windows Home
             var batchScript = new StringBuilder();
             batchScript.AppendLine("@echo off");
+            batchScript.AppendLine("echo [1/2] Verifying and registering Sandbox packages...");
             batchScript.AppendLine("dir /b %SystemRoot%\\servicing\\Packages\\*Containers-DisposableClientVM*.mum >sandbox.txt");
             batchScript.AppendLine("for /f %%i in ('findstr /i . sandbox.txt 2^>nul') do dism /online /norestart /add-package:\"%SystemRoot%\\servicing\\Packages\\%%i\"");
             batchScript.AppendLine("del sandbox.txt");
-            batchScript.AppendLine("Dism /online /enable-feature /featurename:Containers-DisposableClientVM /LimitAccess /ALL");
+            batchScript.AppendLine("echo [2/2] Enabling Windows Sandbox virtual container feature...");
+            batchScript.AppendLine("Dism /online /enable-feature /featurename:Containers-DisposableClientVM /ALL");
 
             try
             {
